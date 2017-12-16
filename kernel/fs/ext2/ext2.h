7239e2f063e8424d2c4f45d6846be9fa4ada9548
Dappur <dapperx@126.com>
2017-12-16 17:55:47 +0800

    Finish the basic test for ext2 filesystem (traversing the directory tree)
    Attention: This commit is transitional. I only focus on the test and the compilation of the other parts may fail.

--- kernel/fs/ext2/ext2.h
+++ kernel/fs/ext2/ext2.h
@@ -1,6 +1,26 @@
 #include "base.h"
 
 #define EXT2_MAX_LEN_NAME 255
+#define EXT2_CNT_BLOCK_PER_INODE 15
+
+//Definition of Reserved Inodes
+#define EXT2_BAD_INO 1//bad blocks inode
+#define EXT2_ROOT_INO 2//root directory inode
+#define EXT2_ACL_IDX_INO 3//ACL index inode (deprecated?)
+#define EXT2_ACL_DATA_INO 4//ACL data inode (deprecated?)
+#define EXT2_BOOT_LOADER_INO 5//boot loader inode
+#define EXT2_UNDEL_DIR_INO 6//undelete directory inode
+
+//Definition of Inode File Type Values
+
+#define EXT2_FT_UNKNOWN 0//Unknown File Type
+#define EXT2_FT_REG_FILE 1//Regular File
+#define EXT2_FT_DIR 2//Directory File
+#define EXT2_FT_CHRDEV 3//Character Device
+#define EXT2_FT_BLKDEV 4//Block Device
+#define EXT2_FT_FIFO 5//Buffer File
+#define EXT2_FT_SOCK 6//Socket File
+#define EXT2_FT_SYMLINK 7//Symbolic Link
 
 struct ext2_superBlock{
 	le32 cnt_inode;
@@ -13,6 +33,7 @@ struct ext2_superBlock{
 	le32 size_fragment;
 	le32 cnt_block_pre_group;
 	le32 cnt_fragment_pre_group;
+	le32 cnt_inode_pre_group;
 	le32 time_mount;
 	le32 time_write;
 	le16 cnt_mount;
@@ -33,6 +54,7 @@ struct ext2_superBlock{
 	le16 group_now;
 	le32 feature_compatible;
 	le32 feature_incompatible;
+	le32 feature_compatible_readonly;
 	u8 uuid[16];
 	char name_volume[16];
 	char dir_mount_last[64];
@@ -42,7 +64,7 @@ struct ext2_superBlock{
 	u8 cnt_dir_prealloc;
 	u16 padding;
 	
-	u32 reserve[190];
+//	u32 reserve[190];
 };
 
 
@@ -70,6 +92,13 @@ struct ext2_inode{
 	le16 cnt_link;
 	le32 cnt_block;//file length, block
 	le32 flag;
+	byte osd1[4];
+	le32 block[EXT2_CNT_BLOCK_PER_INODE];
+	le32 generation;//file version, used for NAS
+	le32 ACL_file;
+	le32 ACL_dir;
+	le32 addr_fragment;
+	byte osd2[12];
 };
 
 
--- kernel/test/testExt2/testExt2.c
+++ kernel/test/testExt2/testExt2.c
@@ -1,19 +1,88 @@
 #include "stdio.h"
+#include "string.h"
 #include "base.h"
 #include "ext2.h"
-#define SIZE_BUFFER 2048
+#define SIZE_BUFFER (1024*50)
 
 static byte buffer[SIZE_BUFFER];
 
-void output_ext2_info(byte *buffer)
+void repeat_char(char c,u32 times)
 {
-	struct ext2_superBlock *superBlock=(struct ext2_superBlock*)&buffer[0];
+	while(times--) putchar(c);
+}
+
+void output_string(char *s,u32 len)
+{
+	for(u32 i=0;i<len;++i) putchar(s[i]);
+}
+
+struct ext2_superBlock* output_ext2_superBlock(byte *buffer)
+{
+	// start from 1KB because of boot sector
+	struct ext2_superBlock *superBlock=(struct ext2_superBlock*)&buffer[1024];
 	printf("cnt_inode: %d\n",superBlock->cnt_inode);
 	printf("cnt_block: %d\n",superBlock->cnt_block);
 	printf("cnt_block_reserve: %d\n",superBlock->cnt_block_reserve);
 	printf("cnt_block_free: %d\n",superBlock->cnt_block_free);
 	printf("cnt_inode_free: %d\n",superBlock->cnt_inode_free);
 	printf("index_data_start: %d\n",superBlock->index_data_start);
+	printf("size_block: %d\n",superBlock->size_block);
+	printf("size_fragment: %d\n",superBlock->size_fragment);
+	printf("cnt_block_pre_group: %d\n",superBlock->cnt_block_pre_group);
+	printf("cnt_fragment_pre_group: %d\n",superBlock->cnt_fragment_pre_group);
+	printf("cnt_inode_pre_group: %d\n",superBlock->cnt_inode_pre_group);
+	printf("time_mount: %d\n",superBlock->time_mount);
+	printf("time_write: %d\n",superBlock->time_write);
+	printf("cnt_mount: %d\n",superBlock->cnt_mount);
+	printf("max_mount: %d\n",superBlock->max_mount);
+	printf("magic: %X\n",superBlock->magic);
+	printf("UUID: ");
+	for(int i=0;i<16;++i) printf("%x",superBlock->uuid[i]);
+	putchar('\n');
+	printf("inode_first: %d\n",superBlock->inode_first);
+
+	printf("size of superBlock: %d\n",sizeof(*superBlock));
+	return superBlock;
+}
+
+struct ext2_inode* get_ext2_inode(byte *buffer,struct ext2_superBlock* superBlock)
+{
+	u32 size_block=(1u<<superBlock->size_block)*1024;
+	printf("size_block: %d\n",size_block);
+	struct ext2_group_descriptor *grpDesc=(struct ext2_group_descriptor*)&buffer[size_block*(superBlock->index_data_start+1)];
+	u32 cnt_grpDesc=(superBlock->cnt_inode+superBlock->cnt_inode_pre_group-1)/superBlock->cnt_inode_pre_group;
+	if(cnt_grpDesc!=(superBlock->cnt_block+superBlock->cnt_block_pre_group-1)/superBlock->cnt_block_pre_group)
+	{
+		puts("*WARNING*: cnt_grpDesc_not_equal");
+	}
+	printf("id_inode_table %d\n",grpDesc->inode_table);
+	struct ext2_inode *inode=(struct ext2_inode*)&buffer[grpDesc->inode_table*size_block];
+	return inode;
+}
+
+void output_ext2_dir(const byte *buffer,const struct ext2_superBlock* superBlock,struct ext2_inode *inode,struct ext2_inode *inode_now,u32 depth)
+{
+	register u32 size_block=(1u<<superBlock->size_block)*1024;
+	// In this simple test, I just think that the directory consists of only one block
+	struct ext2_dir *dir=(struct ext2_dir*)&buffer[inode_now->block[0]*size_block];
+	byte *end_dir=(byte*)dir+inode_now->size;
+	for(;(byte*)dir<end_dir;dir=(struct ext2_dir*)((byte*)dir+dir->size_record))
+	{
+		if(dir->type==EXT2_FT_UNKNOWN) continue;
+
+		repeat_char(' ',depth*4);
+		output_string(dir->name,dir->len_name);
+		// [indent]filename{inode}[filesize]
+		printf(" {%d}[%d]",dir->inode,inode[dir->inode-1].size);
+		putchar('\n');
+
+		if(dir->type==EXT2_FT_DIR)
+		{
+			if(!strcmp(dir->name,".")) continue;
+			if(!strcmp(dir->name,"..")) continue;
+			output_ext2_dir(buffer,superBlock,inode,&inode[dir->inode-1],depth+1);
+		}
+	}
 }
 
 int main()
@@ -24,11 +93,13 @@ int main()
 		fputs("Failed to open img",stderr);
 		goto exit_main;
 	}
-	//start at 1K to skip the boot section
-	fseek(img,1024,SEEK_SET);
 	fread(buffer,1,SIZE_BUFFER,img);
-	output_ext2_info(buffer);
 	fclose(img);
+
+	struct ext2_superBlock* superBlock=output_ext2_superBlock(buffer);
+	struct ext2_inode *inode=get_ext2_inode(buffer,superBlock);
+	output_ext2_dir(buffer,superBlock,inode,&inode[EXT2_ROOT_INO-1],0);
+
 exit_main:
 	return 0;
 }
