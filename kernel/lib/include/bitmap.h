17cf9afe6e8ce16f7c14f11755a39a7b1199ac08
Dappur <dapperx@126.com>
2019-09-17 02:50:56 +0800

    Add the header of bitmap

--- kernel/lib/include/bitmap.h
+++ kernel/lib/include/bitmap.h
@@ -15,10 +15,19 @@ struct bitmap
 // typedef void (*bitmap_type_func_free)(void *addr);
 
 /* Operations for a single bit */
-
+void bitmap_set(bitmap_item *const items, usize index);
+void bitmap_clear(bitmap_item *const items, usize index);
+void bitmap_flip(bitmap_item *const items, usize index);
+bool bitmap_get_value(const bitmap_item *const items, usize index);
 
 /* Operations for multiple bits */
+void bitmap_set_multiple(bitmap_item *const items, const usize begin, const usize cnt);
+void bitmap_clear_multiple(bitmap_item *const items, const usize begin, const usize cnt);
+void bitmap_flip_multiple(bitmap_item *const items, const usize begin, const usize cnt);
 
-/* Finding set or unset bits. */
+/* Operations for statistics */
+usize bitmap_count(const bitmap_item *const items, const usize begin, const usize cnt, const bool value);
+bool bitmap_is_empty(const bitmap_item *const items, usize begin, usize cnt);
+bool bitmap_is_full(const bitmap_item *const items, usize begin, usize cnt);
 
 #endif //_BITMAP_H
